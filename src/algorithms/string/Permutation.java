3bb5b8f1c76bcc839fdca4f522c732c5c06bae9e
rohan shedge <rohan_it05@hotmail.com>
2021-03-07 00:06:39 +0530

    String Permutation

--- src/algorithms/string/Permutation.java
+++ src/algorithms/string/Permutation.java
@@ -4,27 +4,25 @@ public class Permutation {
 	public static void main(String[] args) {
 		Permutation.permute("ABC", 0, 2);
 	}
-	
+
 	private static void permute(String s, int l, int r) {
-		if (l==r) {
+		if (l == r) {
 			System.out.println(s);
-		}
-		else {
-			for(int i=l ; i<=r ; i++) {
+		} else {
+			for (int i = l; i <= r; i++) {
 				s = swap(s, l, i);
-				permute(s, l+1, r);
+				permute(s, l + 1, r);
 				s = swap(s, l, i);
 			}
 		}
 	}
 
-    public static String swap(String a, int i, int j) 
-    { 
-        char temp; 
-        char[] charArray = a.toCharArray(); 
-        temp = charArray[i] ; 
-        charArray[i] = charArray[j]; 
-        charArray[j] = temp; 
-        return String.valueOf(charArray); 
-    } 
+	public static String swap(String a, int i, int j) {
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+	}
 }
