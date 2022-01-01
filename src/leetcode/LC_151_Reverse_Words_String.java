84fc2134d33577459217bf70aacaae63a81d74e3
rohan shedge <rohan_it05@hotmail.com>
2022-01-01 20:49:20 +0530

    reverse words in a string

---
+++ src/leetcode/LC_151_Reverse_Words_String.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class LC_151_Reverse_Words_String {
+
+	private static String reverseWordsString(String s) {
+		Stack<String> st = new Stack<>();
+		String temp = "";
+		String ans = "";
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ') {
+				if (temp.length() > 0)
+					st.push(temp);
+				temp = "";
+			} else
+				temp = temp + s.charAt(i);
+		}
+
+		ans = ans + temp;
+
+		while (!st.isEmpty()) {
+			ans = ans + " " + st.pop();
+		}
+
+		if (ans.length() != 0 && ans.charAt(0) == ' ')
+			ans = ans.substring(1);
+
+		return ans;
+	}
+
+	// Driver Code
+	public static void main(String[] args) {
+		String s = "     Rohan is a bad boy        ";
+		System.out.println("Reverse of the string is => " + reverseWordsString(s));
+	}
+}
