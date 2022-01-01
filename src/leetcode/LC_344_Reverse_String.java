d8a15ae19389662c980ade93f62cd0ca33c11f1b
rohan shedge <rohan_it05@hotmail.com>
2022-01-01 16:04:21 +0530

    Reverse String

---
+++ src/leetcode/LC_344_Reverse_String.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class LC_344_Reverse_String {
+	
+	private static char[] reverse(char[] s) {
+		
+		int l = 0;
+		int r = s.length - 1;
+		char temp;
+		while(l < r) {
+			temp = s[l];
+			s[l] = s[r];
+			s[r] = temp;
+			l++;
+			r--;
+		}
+		return s;
+	}
+	
+	@Test
+	public void test() {
+		assertArrayEquals(reverse(new char[] {'R','o','h','a','n'}), new char[] {'n','a','h','o','R'});
+	}
+}
